24489507613862562dd4ae31238143faea05f7b2
unknown <islandasilvapeixoto@gmail.com>
2023-11-21 19:37:55 -0400

    aqui vai minha atividade

---
+++ Atividade45.java
@@ -0,0 +1,28 @@
+package ListaExercicios;
+
+import java.util.Scanner;
+
+public class Atividade45 {
+	
+
+
+	    public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+
+	        System.out.print("Qual o tamanho N do array que você deseja? ");
+	        int N = sc.nextInt();
+	        ArrayNumeroInteiro array = new ArrayNumeroInteiro(N);
+
+	        for (int i = 0; i < N; i++) {
+	            System.out.print("Digite o " + (i+1) + "º número: ");
+	            int num = sc.nextInt();
+	            array.adicionar(num);
+	        }
+	        System.out.println("A média aritmética é: " + array.calculaMedia());
+
+	        sc.close();
+	    }
+	}
+
+
+
